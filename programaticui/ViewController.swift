884a5d6620508e35a2e9e10c360fa8766241b8ca
Erxhan Selmani <ese@radionomy.com>
2018-10-26 14:40:04 +0200

    Initial Commit

---
+++ programaticui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programaticui
+//
+//  Created by Exrhan Selmani on 26/10/2018.
+//  Copyright © 2018 Exrhan Selmani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
